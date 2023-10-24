b6befc6b5702792726488903dcd44a3f660dd698
Felipe Guimarães <54296382+felipe2g@users.noreply.github.com>
2023-10-23 21:39:01 -0300

    Add files via upload

---
+++ trabalho-lock-otimista/src/main/java/com/example/trabalholockotimista/Services/ContaService.java
@@ -0,0 +1,50 @@
+package com.example.trabalholockotimista.Services;
+
+import com.example.trabalholockotimista.Entities.Conta;
+import com.example.trabalholockotimista.Repositories.ContaRepository;
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
+
+@Service
+public class ContaService {
+    @Autowired
+    private ContaRepository contaRepository;
+
+    @Transactional
+    public Conta sacar(String numeroConta, BigDecimal valor) throws Exception {
+        Conta conta = contaRepository.findByNumeroConta(numeroConta);
+
+        if (conta == null) {
+            throw new Exception("Conta não encontrada");
+        }
+
+        if (conta.getSaldo().compareTo(valor) < 0) {
+            throw new Exception("Saldo insuficiente.");
+        }
+
+        conta.setSaldo(conta.getSaldo().subtract(valor));
+        contaRepository.save(conta);
+        return conta;
+    }
+
+    @Transactional
+    public Conta depositar(String numeroConta, BigDecimal valor) throws Exception {
+        Conta conta = contaRepository.findByNumeroConta(numeroConta);
+        if (conta == null) {
+            Conta novaConta = new Conta();
+            novaConta.setNumeroConta(numeroConta);
+            novaConta.setSaldo(valor);
+
+            contaRepository.save(novaConta);
+
+            return novaConta;
+        }
+        BigDecimal novoSaldo = conta.getSaldo().add(valor);
+        conta.setSaldo(novoSaldo);
+        contaRepository.save(conta);
+        return conta;
+    }
+}
